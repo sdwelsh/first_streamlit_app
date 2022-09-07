470348c6bec704f74d73cbdbb5ed504bb24164c5
sdwelsh <65737619+sdwelsh@users.noreply.github.com>
2022-09-07 10:31:51 -0400

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas 
 
-my_fruit_list = pandas.read_csv("https://uni
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("My Parents new healthy diner")
 streamlit.header('Breakfast Menu')
